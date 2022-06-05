0d57dc335b07ca3e30e4c658a337727b6eb7ff2c
Mohammad Jahid <mohammadjahid0007@gmail.com>
2022-06-05 08:04:15 +0600

    summation

--- assignment.py
+++ assignment.py
@@ -1,7 +1,25 @@
 # Assignment by Mohammad Jahid
 
 # experiment 1
-print('Do Not Adopt Unfairmeans in  the exam')
+# print('Do Not Adopt Unfairmeans in  the exam')
 
 # experiment 2 
-print('Welcome to python programming') 
+# print('Welcome to python programming') 
+
+
+# experiment 3-Find the sum of two numbers
+# Number1 = int(input('Enter the first number: '))
+# Number2 = int(input('Enter the second number: '))
+# sum = Number1 + Number2
+# print('Summation is: ' , sum)
+
+# experiment 4- find the summation of two float numbers
+Number1 = float(input('Enter the first number: '))
+Number2 = float(input('Enter the second number: '))
+sum = Number1 + Number2
+print('Summation is: ' , sum)
+
+
+
+
+
